aa055ab51ab71cf6e7d482afbe57ff00a83ef249
Abdifatah <abdifatah4@msn.com>
2025-03-10 10:46:09 +0100

    cors added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import router from './router'
 import "dotenv/config";
-
+import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.json());
 app.use("/api", router);
 
