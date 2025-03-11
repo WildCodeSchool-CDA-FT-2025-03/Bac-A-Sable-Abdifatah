5012594ef00a46fa893e9bdb75096235bb6e543f
Abdifatah <abdifatah4@msn.com>
2025-03-11 16:37:57 +0100

    select + searchParam

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,28 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import { ReposCard } from '../components/RepoCard';
 import { useRepos } from '../services/repos';
 function Home() {
-  const { repos } = useRepos();
+  const { repos, getAllRepos } = useRepos();
+  const [searchParam, setSearchParam] = useSearchParams();
+    useEffect(() => {
+    getAllRepos(searchParam.get('limit') || '5');
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchParam]);
+
   return (
     <>
       <p>Liste de Repo depuis mon github</p>
+      <div className="box">
+      <label htmlFor="">
+         Nombre de repos affichés
+        <select value={searchParam.get('limit') || '5'} name="limit" id="" onChange={(e) => setSearchParam({ limit: e.target.value })}>
+          <option value="5" >5</option>
+          <option value="10" >10</option>
+          <option value="15" >15</option>
+        </select>
+      </label>
+      </div>
       <div className="repoList">
         {
           repos.length > 0 && repos.map((repo) => (
--- client/src/services/repos.ts
+++ client/src/services/repos.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Repo } from "../types/repos.type";
 import { client } from "./client";
 
@@ -6,32 +6,27 @@ export const useRepos = () => {
   const [repos, setRepos] = useState<Repo[]>([]);
   const [oneRepos, setOneRepos] = useState<Repo>();
 
-  const getAllRepos = async () => {
-    client.get("/repos")
+  const getAllRepos = async (limit: string) => {
+    client
+      .get(`/repos?limit=${limit}`)
       .then((repos) => {
         setRepos(repos.data);
       })
       .catch((error) => {
-        console.error(error)
-      })
+        console.error(error);
+      });
   };
 
   const getOneRepos = (id: string) => {
-    client.get(`/repos/${id}`).then((repo) => {
-      setOneRepos(repo.data as Repo);
-    })
-      .catch((error) => {
-        console.error(error)
+    client
+      .get(`/repos/${id}`)
+      .then((repo) => {
+        setOneRepos(repo.data as Repo);
       })
-  }
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-  useEffect(() => {
-    getAllRepos();
-  }, []);
-  return { repos, oneRepos, getOneRepos };
+  return { repos, oneRepos, getOneRepos, getAllRepos };
 };
-
-
-
-
-
