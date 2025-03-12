ccbc6a76a5cfe1f8ccb9e4900719d7e154ecfa5c
Abdifatah <abdifatah4@msn.com>
2025-03-12 15:59:54 +0100

    get all languages

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,13 +1,14 @@
 import express, {Request, Response} from 'express';
 import repos from './repos/repos.controller';
+import languages from "./languages/languages.controller";
 
 const router = express.Router();
 
-router.get('/', (_, res) => {
-    res.status(200).send('Hello from router !');
+router.get("/", (_, res) => {
+  res.status(200).send("Hello from router !");
 });
 
-
 router.use("/repos", repos);
+router.use("/languages", languages);
 
 export default router;
