1113d0f25546d2d947126e033e0270d211a01a16
Abdifatah <abdifatah4@msn.com>
2025-03-12 17:21:10 +0100

    add isPrivate to repos validation

--- api/src/repos/repos.validation.ts
+++ api/src/repos/repos.validation.ts
@@ -2,16 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import Joi from "joi";
 
 const createRepoSchema = Joi.object({
-    name: Joi.string(),
-    url: Joi.string().required(),
-    languages: Joi.array().items(
-        Joi.object({
-            size: Joi.number().required(),
-            node: Joi.object({
-                name: Joi.string().required(),
-            }).required(),
-        })
-    ),
+  name: Joi.string(),
+  url: Joi.string().required(),
+  languages: Joi.array().items(
+    Joi.object({
+      size: Joi.number().required(),
+      node: Joi.object({
+        name: Joi.string().required(),
+      }).required(),
+    })
+  ),
+  isPrivate: Joi.bool(),
 });
 
 const validateRepo = (req: Request, res: Response, next:NextFunction) => {
