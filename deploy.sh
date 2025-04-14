06d59bcf1b072455e8ffb5680b7f774995ecbcb7
Abdifatah <abdifatah4@msn.com>
2025-04-14 21:24:38 +0200

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git switch main # On force la bascule sur notre branche de référence (main en cas de merge)
-git pull origin main # On met à jour la branche
-
+# git pull origin main # On met à jour la branche
+git pull https://$GH_TOKEN@ggithub.com/WildCodeSchool-CDA-FT-2025-03/Bac-A-Sable-Abdifatah.git main
 
 docker stop $(docker ps -a -q) # On arrête tous les containers en cours
 
